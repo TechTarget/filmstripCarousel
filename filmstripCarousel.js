93169fe37347e3ef5419f0769993d556d229db15
Morgan Wigmanich <okize123@gmail.com>
2012-06-15 06:37:47 -0400

    early working version; lots to fix

--- filmstripCarousel.js
+++ filmstripCarousel.js
@@ -3,21 +3,6 @@
  * Copyright (c) 2012 | Licensed under the MIT license - http://www.opensource.org/licenses/mit-license.php
  */
 
-/*
- * Plugin: Filmstrip Carousel
- * Version: 0.7 (June 11th 2012)
- * Description:
- * Author: Morgan Wigmanich, TechTarget
- * Options:
- *   autoPlay: boolean; this will automagically rotate through the content list
- *   autoPlaySpeed: integer; the speed (in ms) at which the autoplay will rotate through the items
- *   mouseEvent: string; which mouse event will trigger the content switch; currently supported: hover & click
- *   switchSpeed: integer; the speed (0 - ∞ in ms) that it takes to switch to displaying a new contentItem
- *   equalizeHeights: boolean; equalizes the heights of the content container and navigation list to whichever is largest
- * Dependencies: none
- */
-
-
 ;(function ( $, window, undefined ) {
 
 	'use strict';
@@ -44,7 +29,204 @@
 		// plugin vars
 		var o = this.options;
 		var filmstrip = $(this.element);
+		var filmstripWindow = filmstrip.children('.filmstripWindow');
+		var filmstripWidth = filmstripWindow.outerWidth();
+		var itemsContainer = filmstripWindow.children('ul');
+		var items = itemsContainer.find('> li');
+		var itemWidth = items.outerWidth();
+		var itemCount = items.size();
+		var itemsContainerWidth = itemWidth * itemCount;
 		
+		//var firstItem = items[0];
+		//var lastItem = items[itemCount - 1];
+		//var filmstripIsMoving = false;
+		
+		// depending on where on the page the filmstrip is located, it will display a different amount of items
+		// this is determined by dividing the width of the filmstrip window by the width of the items and rounding up a bit
+		var itemsToShow = Math.round(filmstripWidth/itemWidth);
+		
+		// @todo; I don't understand why this is necessary, but filmstripWidth was off by 80px when there was only one item showing
+		// probably related to the width of the nav buttons; also note: changing filmstripWidth to filmstrip.outerWidth() fixes the issue
+		// but screws up the widths in carousels with multiple items showing
+		filmstripWidth = (itemsToShow === 1) ? itemWidth : filmstripWindow.outerWidth();
+				
+		//var movement = itemsToShow * itemWidth;
+		var itemGroups = Math.ceil(itemCount/itemsToShow);
+		var itemGroupToShow = 0;
+				
+		var navigation = {}; // the navigation object
+		var pagination = {}; // the pagination object
+
+		// creates the html that compromises the navigation elements
+		// inserts itself into dom and binds event handlers to button elements
+		var addNavigation = function() {
+			
+			// @todo
+			navigation.btnPrev = $('<a>', {
+				'class': 'filmstripPrevious disabled',
+				href: '#',
+				title: 'Previous',
+				text: 'Previous'
+			}).on('click', function (e) {
+				e.preventDefault();
+				if ( !$(this).hasClass('disabled') ) {
+					filmstrip.trigger('filmstrip.move', 'previous');
+				}
+			});
+
+			// @todo
+			navigation.btnNext = $('<a>', {
+				'class': 'filmstripNext',
+				href: '#',
+				title: 'Next',
+				text: 'Next'
+			}).on('click', function (e) {
+				e.preventDefault();
+				if ( !$(this).hasClass('disabled') ) {
+					filmstrip.trigger('filmstrip.move', 'next');
+				}
+			});
+			
+			// @todo
+			navigation.container = $('<div/>', {
+				'class': 'filmstripNavigation'
+			}).append(navigation.btnPrev, navigation.btnNext);
+
+			// @todo
+			filmstrip.append(navigation.container);
+
+		};
+		
+		// @todo
+		var addPagination = function() {
+			
+			var groupIndex = 0;
+			
+			var items = [];
+			var className = ['active'];
+
+			// @todo
+			for (var i = 0; i < itemGroups; i++) {
+				items.push('<a href="#" class="' + (className[i] || ' ') + '" data-filmstrip-group="' + i + '">' + (i+1) + '</a>');
+			}
+			
+			pagination.container = $('<div/>', {
+				'class': 'filmstripPagination'
+			}).on('click', 'a', function (e) {
+				groupIndex = $(this).data('filmstripGroup');
+				e.preventDefault();
+				filmstrip.trigger('filmstrip.move', groupIndex);
+			});
+			
+			pagination.items = $(items);
+			
+			// @todo
+			filmstrip.append(pagination.container.append(items.join('')));
+			
+		};
+		
+		// @todo
+		var moveStrip = function(e, direction) {
+			
+			// @todo
+			var mover = function() {
+				itemsContainer.css('left', -filmstripWidth*itemGroupToShow);
+			};
+			
+			// @todo
+			var selectDot = function(index) {
+					
+					// @todo fix var name
+					var tmp = pagination.container.find('a');
+					tmp.removeClass('active');
+					tmp.eq(index).addClass('active');
+	
+			};
+			
+			// direction is overloaded & can either be a number (item group index) or a string (next/previous)
+			if (typeof direction === 'number') {
+													
+				// @todo
+				if (o.pagination) {
+					selectDot(direction);
+					itemGroupToShow = direction;
+					mover();
+				}
+				
+			} else {
+					
+				// this prevents queue buildup as the filmstrip is shifting position
+				//if (!filmstripIsMoving) {
+							
+					if (direction === 'previous' && itemGroupToShow > 0) {
+						//filmstripIsMoving = true;
+						itemGroupToShow--;
+						mover();
+					}
+					
+					if (direction === 'next' && itemGroupToShow < itemGroups - 1) {
+						//filmstripIsMoving = true;
+						itemGroupToShow++;
+						mover();
+					}
+					
+					
+					if (o.pagination) {
+						selectDot(itemGroupToShow);
+					}
+				
+				//}
+				
+			}
+			
+			// @todo this sucks; rewrite this next
+			if (o.navigation) {
+
+				if (itemGroupToShow === 0) {
+					navigation.btnPrev.addClass('disabled');
+				} else {
+					navigation.btnPrev.removeClass('disabled');
+				}
+				if (itemGroupToShow === itemGroups - 1) {
+					navigation.btnNext.addClass('disabled');
+				} else {
+					navigation.btnNext.removeClass('disabled');
+				}
+			}
+
+		};
+		
+		// initialize plugin:
+		
+		// if there are no items, there is nothing to be done
+		if (itemCount === 0) {
+			filmstrip.remove();
+			return;
+		}
+		
+		// adjust width of filmstrip list to contain all the items
+		itemsContainer.width(itemsContainerWidth);
+		
+		// if navigation is enabled add nav object to the dom
+		// if disabled add class to container to indicate such (for css styling purposes)
+		if (o.navigation) {
+			addNavigation();
+		} else {
+			filmstrip.addClass('filmstripNoNav');
+		}
+		
+		// if pagination is enabled add nav object to the dom
+		// if disabled add class to container to indicate such (for css styling purposes)
+		if (o.pagination) {
+			addPagination();
+		} else {
+			filmstrip.addClass('filmstripNoPagination');
+		}
+			
+		// bind handlers
+		filmstrip.on('filmstrip.move', moveStrip);
+			
+				
 	};
 
 	// a lightweight plugin wrapper around the constructor preventing against multiple instantiations
