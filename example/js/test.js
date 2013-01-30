3b997ed523611bdee9e3bafdb6bcec5ac4159d2b
Morgan Wigmanich <okize123@gmail.com>
2013-01-30 18:04:02 -0500

    fixed tabs

--- example/js/test.js
+++ example/js/test.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function() {
 
-	'use strict';
+  'use strict';
 
   Holder.run({
     domain: 'example.com',
@@ -13,65 +13,65 @@ $(document).on('ready', function() {
       }
     });
 
-	$('#fsInline1').filmstripCarousel({
-		navigationPosition: 'Inline',
+  $('#fsInline1').filmstripCarousel({
+    navigationPosition: 'Inline',
     autoplay: true,
     autoplaySpeed: 5000
-	});
+  });
 
-	$('#fsInline2').filmstripCarousel({
-		navigationPosition: 'Inline'
-	});
+  $('#fsInline2').filmstripCarousel({
+    navigationPosition: 'Inline'
+  });
 
-	$('#fsInline3').filmstripCarousel({
-		navigationPosition: 'Inline'
-	});
+  $('#fsInline3').filmstripCarousel({
+    navigationPosition: 'Inline'
+  });
 
-	$('#fsInline4').filmstripCarousel({
-		pagination: false,
-		navigationPosition: 'Inline'
-	});
+  $('#fsInline4').filmstripCarousel({
+    pagination: false,
+    navigationPosition: 'Inline'
+  });
 
-	$('#fsInline5').filmstripCarousel({
-		navigation: false,
-		navigationPosition: 'Inline'
-	});
+  $('#fsInline5').filmstripCarousel({
+    navigation: false,
+    navigationPosition: 'Inline'
+  });
 
-	$('#fsInline6').filmstripCarousel({
-		pagination: false,
-		navigation: false
-	});
+  $('#fsInline6').filmstripCarousel({
+    pagination: false,
+    navigation: false
+  });
 
-	$('#fsOutside1').filmstripCarousel({
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside1').filmstripCarousel({
+    navigationPosition: 'Outside'
+  });
 
-	$('#fsOutside2').filmstripCarousel({
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside2').filmstripCarousel({
+    navigationPosition: 'Outside'
+  });
 
-	$('#fsOutside3').filmstripCarousel({
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside3').filmstripCarousel({
+    navigationPosition: 'Outside'
+  });
 
-	$('#fsOutside4').filmstripCarousel({
-		pagination: false,
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside4').filmstripCarousel({
+    pagination: false,
+    navigationPosition: 'Outside'
+  });
 
-	$('#fsOutside5').filmstripCarousel({
-		navigation: false,
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside5').filmstripCarousel({
+    navigation: false,
+    navigationPosition: 'Outside'
+  });
 
-	$('#fsOutside6').filmstripCarousel({
-		pagination: false,
-		navigation: false
-	});
+  $('#fsOutside6').filmstripCarousel({
+    pagination: false,
+    navigation: false
+  });
 
-	$('#fsOutside').filmstripCarousel({
-		navigationPosition: 'Outside'
-	});
+  $('#fsOutside').filmstripCarousel({
+    navigationPosition: 'Outside'
+  });
 
 });
 
